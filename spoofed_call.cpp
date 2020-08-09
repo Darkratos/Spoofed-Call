2466700521e9baba329063106a96834249c22f7e
Darkratos <49787965+Darkratos@users.noreply.github.com>
2020-08-09 17:40:31 -0300

    Update spoofed_call.cpp

--- spoofed_call.cpp
+++ spoofed_call.cpp
@@ -11,165 +11,170 @@ spoofed_call::spoofed_call( spoof_method method, uintptr_t call_esi )
 	}
 }
 
-int spoofed_call::cdecl_fn( uintptr_t address, int param_count, ... )
+__declspec( naked ) int spoofed_call::cdecl_fn( uintptr_t address, int param_count, ... )
 {
-	uintptr_t call_esi = this->call_esi;
-
 	__asm
-	{
-		push esp					// Save some registers
+	{	
 		push ebp
-		push esi
+		mov ebp, esp							
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Spoof the current return with a simple xor
+		push esi								// Save some registers
 		push edi
 		push ecx
 
-		lea esi, param_count		// Put the address of param_count on esi
-		mov ecx, [ esi ]			// Save param_count on ecx
-		mov edi, ecx				// Save it also in edi, so we can clean up the stack after
-		add esi, 4					// Add 4 so esi has the parameters
-		cmp ecx, 0					// Check if it has parameters
-		je after					// If it doesn't, skip the pushes
+		lea esi, param_count					// Put the address of param_count on esi
+		mov ecx, [ esi ]						// Save param_count on ecx
+		mov edi, ecx							// Save it also in edi, so we can clean up the stack after
+		test ecx, ecx							// Check if there are parameters to be pushed
+		je after								// There aren't, so we skip the pushes
 
-	push_params:
-		dec ecx
-		mov eax, [ esi + ecx * 4 ]	// Get the params in the reverse order
-		push eax					// And push them in the new stack
-		cmp ecx, 0
-		jne push_params				// Loop back until all params are pushed
+	push_params:	
+		mov eax, [ esi + ecx * 4 ]				// Get the params in the reverse order
+		push eax								// And push them in the new stack
+		loop push_params						// Loop back until all params are pushed
 
 	after:
-		push call_esi				// Push the fake return address
-		mov esi, local_return		// Put the local_return on esi
-		jmp [ address ]				// Jump to the function
+		mov eax, [ ebp + 8 ]					// Get the fake return address from the stack
+		push [ eax ]							// Push the fake return address
+		mov esi, local_return					// Put the local_return on esi
+		jmp [ address ]							// Jump to the function
 
 	local_return:
-		add esp, 4					// Clean up the stack after the call
-		mov ecx, edi
-		imul ecx, 4
-		add esp, ecx
-		pop ecx						// Restore the saved registers
+		inc edi									// Increase one so we include the new return address
+		imul edi, 4								// Multiply by 4 to get the size to be cleaned
+		add esp, edi							// Clean up the stack
+
+		pop ecx									// Restore the saved registers
 		pop edi
 		pop esi
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Restore the current return
+		mov esp, ebp
 		pop ebp
-		pop esp
+		retn
 	}
 }
 
-int spoofed_call::stdcall_fn( uintptr_t address, int param_count, ... )
+__declspec( naked ) int spoofed_call::stdcall_fn( uintptr_t address, int param_count, ... )
 {
-	uintptr_t call_esi = this->call_esi;
-
 	__asm
 	{
-		push esp					// Save some registers
 		push ebp
-		push esi
+		mov ebp, esp
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Spoof the current return with a simple xor
+		push esi								// Save some registers
+		push edi
 		push ecx
 
-		lea esi, param_count		// Put the address of param_count on esi
-		mov ecx, [ esi ]			// Save param_count on ecx
-		add esi, 4					// Add 4 so esi has the parameters
-		cmp ecx, 0					// Check if it has parameters
-		je after					// If it doesn't, skip the pushes
+		lea esi, param_count					// Put the address of param_count on esi
+		mov ecx, [ esi ]						// Save param_count on ecx
+		mov edi, ecx							// Save it also in edi, so we can clean up the stack after
+		test ecx, ecx							// Check if there are parameters to be pushed
+		je after								// There aren't, so we skip the pushes
 
 	push_params:
-		dec ecx
-		mov eax, [ esi + ecx * 4 ]	// Get the params in the reverse order
-		push eax					// And push them in the new stack
-		cmp ecx, 0
-		jne push_params				// Loop back until all params are pushed
+		mov eax, [ esi + ecx * 4 ]				// Get the params in the reverse order
+		push eax								// And push them in the new stack
+		loop push_params						// Loop back until all params are pushed
 
 	after:
-		push call_esi				// Push the fake return address
-		mov esi, local_return		// Put the local_return on esi
-		jmp [ address ]				// Jump to the function
+		mov eax, [ ebp + 8 ]					// Get the fake return address from the stack
+		push [ eax ]							// Push the fake return address
+		mov esi, local_return					// Put the local_return on esi
+		jmp [ address ]							// Jump to the function
 
 	local_return:
-		add esp, 4					// Clean up the stack after the call
-		pop ecx						// Restore the saved registers
+		add esp, 4								// Clean up the stack
+		pop ecx									// Restore the saved registers
+		pop edi
 		pop esi
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Restore the current return
+		mov esp, ebp
 		pop ebp
-		pop esp
+		retn
 	}
 }
 
-int spoofed_call::thiscall_fn( uintptr_t address, uintptr_t ecx_value, int param_count, ... )
+__declspec( naked ) int spoofed_call::thiscall_fn( uintptr_t address, uintptr_t ecx_value, int param_count, ... )
 {
-	uintptr_t call_esi = this->call_esi;
-
 	__asm
 	{
-		push esp					// Save some registers
 		push ebp
-		push esi
+		mov ebp, esp
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Spoof the current return with a simple xor
+		push esi								// Save some registers
+		push edi
 		push ecx
 
-		lea esi, param_count		// Put the address of param_count on esi
-		mov ecx, [ esi ]			// Save param_count on ecx
-		add esi, 4					// Add 4 so esi has the parameters
-		cmp ecx, 0					// Check if it has parameters
-		je after					// If it doesn't, skip the pushes
+		lea esi, param_count					// Put the address of param_count on esi
+		mov ecx, [ esi ]						// Save param_count on ecx
+		mov edi, ecx							// Save it also in edi, so we can clean up the stack after
+		test ecx, ecx							// Check if there are parameters to be pushed
+		je after								// There aren't, so we skip the pushes
 
 	push_params:
-		dec ecx
-		mov eax, [ esi + ecx * 4 ]	// Get the params in the reverse order
-		push eax					// And push them in the new stack
-		cmp ecx, 0					// Loop back until all params are pushed
-		jne push_params
+		mov eax, [ esi + ecx * 4 ]				// Get the params in the reverse order
+		push eax								// And push them in the new stack
+		loop push_params						// Loop back until all params are pushed
 
 	after:
-		push call_esi				// Push the fake return address
-		mov esi, local_return		// Put the local_return on esi
-		mov ecx, ecx_value			// Parse the 'this' to ecx
-		jmp [ address ]				// Jump to the function
+		mov eax, [ ebp + 8 ]					// Get the fake return address from the stack
+		push [ eax ]							// Push the fake return address
+		mov esi, local_return					// Put the local_return on esi
+		mov ecx, [ ebp + 0x10 ]					// Put the ecx value
+		jmp [ address ]							// Jump to the function
 
 	local_return:
-		add esp, 4					// Clean up the stack after the call
-		pop ecx						// Restore the saved registers
+		add esp, 4								// Clean up the stack
+		pop ecx									// Restore the saved registers
+		pop edi
 		pop esi
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Restore the current return
+		mov esp, ebp
 		pop ebp
-		pop esp
+		retn
 	}
 }
 
-int spoofed_call::fastcall_fn( uintptr_t address, uintptr_t ecx_value, uintptr_t edx_value, int param_count, ... )
+__declspec( naked ) int spoofed_call::fastcall_fn( uintptr_t address, uintptr_t ecx_value, uintptr_t edx_value, int param_count, ... )
 {
-	uintptr_t call_esi = this->call_esi;
-
 	__asm
 	{
-		push esp					// Save some registers
 		push ebp
-		push esi
+		mov ebp, esp
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Spoof the current return with a simple xor
+		push esi								// Save some registers
+		push edi
 		push ecx
 		push edx
 
-		lea esi, param_count		// Put the address of param_count on esi
-		mov ecx, [ esi ]			// Save param_count on ecx
-		add esi, 4					// Add 4 so esi has the parameters
-		cmp ecx, 0					// Check if it has parameters
-		je after					// If it doesn't, skip the pushes
+		lea esi, param_count					// Put the address of param_count on esi
+		mov ecx, [ esi ]						// Save param_count on ecx
+		mov edi, ecx							// Save it also in edi, so we can clean up the stack after
+		test ecx, ecx							// Check if there are parameters to be pushed
+		je after								// There aren't, so we skip the pushes
 
 	push_params:
-		dec ecx
-		mov eax, [ esi + ecx * 4 ]	// Get the params in the reverse order
-		push eax					// And push them in the new stack
-		cmp ecx, 0					// Loop back until all params are pushed
-		jne push_params
+		mov eax, [ esi + ecx * 4 ]				// Get the params in the reverse order
+		push eax								// And push them in the new stack
+		loop push_params						// Loop back until all params are pushed
 
 	after:
-		push call_esi				// Push the fake return address
-		mov esi, local_return		// Put the local_return on esi
-		mov ecx, ecx_value			// Parse the ecx value
-		mov edx, edx_value			// Parse the edx value
-		jmp [ address ]				// Jump to the function
+		mov eax, [ ebp + 8 ]					// Get the fake return address from the stack
+		push [ eax ]							// Push the fake return address
+		mov esi, local_return					// Put the local_return on esi
+		mov ecx, [ ebp + 0x10]					// Put the ecx value
+		mov edx, [ ebp + 0x14 ]					// Put the edx value
+		jmp [ address ]							// Jump to the function
 
 	local_return:
-		add esp, 4					// Clean up the stack after the call
-		pop edx						// Restore the saved registers
+		add esp, 4								// Clean up the stack
+		pop edx									// Restore the saved registers
 		pop ecx
+		pop edi
 		pop esi
+		xor dword ptr[ ebp + 4 ], 0xDEADBEEF	// Restore the current return
+		mov esp, ebp
 		pop ebp
-		pop esp
+		retn
 	}
 }
